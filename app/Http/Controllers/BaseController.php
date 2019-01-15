4babdbe4ff581bacedd6fd3bb8dfd0289797a39a
Aline Chaves <aline.chaves.dev@gmail.com>
2019-01-15 14:59:09 -0300

    class of response

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -3,10 +3,7 @@
 
 namespace App\Http\Controllers;
 
-
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller as Controller;
-
 
 class BaseController extends Controller
 {
@@ -18,7 +15,7 @@ class BaseController extends Controller
     public function sendResponse($result, $message)
     {
         $response = [
-            'success' => true,
+            'status' => true,
             'data'    => $result,
             'message' => $message,
         ];
@@ -36,7 +33,7 @@ class BaseController extends Controller
     public function sendError($error, $errorMessages = [], $code = 404)
     {
         $response = [
-            'success' => false,
+            'status' => false,
             'message' => $error,
         ];
 
